819ad77948a59bc0dfab0aee6cc098199fa6abf4
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-11-25 06:59:36 -0500

    mock fe for prepractice

---
+++ front-end/src/components/Form.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Form() {
+
+    const [user, setUser] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        city:"",
+        state:"",
+        github_url:"",
+    })
+
+    function handleChange(event) {
+        console.log(user)
+        setUser({...user, [event.target.name]: event.target.value})
+    }
+
+    // function handleSubmit(event) {
+    //     // axios.post(user)
+    // }
+
+    return (
+        <div className="Form">
+        <form>
+            <input type="text" name="first_name" placeholder="First Name" value={user.first_name} onChange={handleChange}></input>
+            <input type="text" name="last_name" placeholder="Last Name" value={user.last_name} onChange={handleChange}></input>
+            <input type="text" name="email" placeholder="email" value={user.email} onChange={handleChange}></input>
+            <input type="text" name="password" placeholder="password" value={user.password} onChange={handleChange}></input>
+            <input type="text" name="city" placeholder="city" value={user.city} onChange={handleChange}></input>
+            <input type="text" name="state" placeholder="state" value={user.state} onChange={handleChange}></input>
+            <input type="text" name="github_url" placeholder="gtihub url" value={user.github_url} onChange={handleChange}></input>
+            <button type="submit">Sign Up</button>
+        </form>
+        </div>
+    );
+}
+
+export default Form;
